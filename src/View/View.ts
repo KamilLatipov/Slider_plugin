3ade9ead2900f0db38c0f93aeb309b7126ed6182
KamilLatipov <latipovkamil@mail.ru>
2020-07-30 12:07:26 +0300

    Исправил вертикальную/горизонтальную ориентацию

--- src/View/View.ts
+++ src/View/View.ts
@@ -3,6 +3,7 @@ export default class View {
   slider: HTMLElement;
   scale: HTMLElement;
   pointer: HTMLElement;
+  orientation: string = 'horizontal';
 
   constructor(root: HTMLElement) {
     this.root = root;
@@ -22,14 +23,12 @@ export default class View {
     this.slider.appendChild(this.pointer);
 
     this.pointer.addEventListener('mousedown', this.drag);
-    this.pointer.ondragstart = function() {
+    this.pointer.ondragstart = function () {
       return false;
     };
   }
 
-  private drag = (event: any) => {
-    this.moveAt(event.clientX);
-
+  private drag = () => {
     document.addEventListener('mousemove', this.onMouseMove);
     document.addEventListener('mouseup', this.unDrag);
   };
@@ -38,22 +37,36 @@ export default class View {
     document.removeEventListener('mousemove', this.onMouseMove);
   };
 
-  private moveAt  = (pageX: any) => {
-    let shiftX = pageX - this.slider.getBoundingClientRect().left;
-    let position = shiftX - this.pointer.offsetWidth / 2;
-
-    if (position + this.pointer.offsetWidth > this.slider.offsetWidth) {
-      this.pointer.style.left = this.slider.offsetWidth - this.pointer.offsetWidth  + 'px';
-    }
-    else if (position  < 0) {
-      this.pointer.style.left = 0 + 'px';
-    }
-    else {
-      this.pointer.style.left = position + 'px';
+  private moveAt = (XCoordinate: any, YCoordinate: any, orientation: any) => {
+    if (orientation == 'horizontal') {
+      this.pointer.style.left = XCoordinate + 'px';
+    } else {
+      this.pointer.style.top = YCoordinate + 'px';
     }
   };
 
   private onMouseMove = (event: any) => {
-    this.moveAt(event.clientX);
+    let shiftX = (event.clientX - this.slider.getBoundingClientRect().left) - this.pointer.offsetWidth / 2;
+    let shiftY = (event.clientY - this.slider.getBoundingClientRect().top) - this.pointer.offsetHeight / 2;
+
+    let XCoordinate = this.toLimit(this.slider.offsetWidth, 0, shiftX);
+    let YCoordinate = this.toLimit(this.slider.offsetHeight, 0, shiftY);
+
+    this.moveAt(XCoordinate, YCoordinate, this.orientation);
+  };
+
+  public setOrientation(orientation: string) {
+    if (orientation == 'horizontal') {
+      this.orientation = 'horizontal';
+      this.slider.classList.add('slider__horizontal');
+    } else {
+      this.orientation = 'vertical';
+      this.pointer.style.left = 0 + 'px';
+      this.slider.classList.add('slider__vertical');
+    }
+  }
+
+  private toLimit(max: number, min: number, value: number) {
+    return Math.min(Math.max(min, value), max);
   };
 }
--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,12 @@ let root = document.getElementById('2d');
 console.log(root);
 
 let Slider = new View(root);
+
+let button = document.createElement('button');
+button.classList.add('button');
+document.body.append(button);
+
+button.onclick = function()  {
+  Slider.setOrientation('vertical');
+};
+

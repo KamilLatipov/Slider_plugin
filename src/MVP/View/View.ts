1ce1f7098ec98d614c7e59525a7f8aca38a915b1
Kamil <latipovkamil@mail.ru>
2021-09-02 21:26:08 +0300

    Updated View initialize method

--- src/MVP/View/View.ts
+++ src/MVP/View/View.ts
@@ -1,19 +1,30 @@
-import { Track } from './Track.';
+import { Track } from './Track';
+import { Thumb } from './Thumb';
+import { ProgressBar } from './ProgressBar';
 
 class View {
     parentElement: HTMLElement;
     sliderElement: HTMLElement;
     track: Track;
+    thumb: Thumb;
+    progressBar: ProgressBar;
     constructor(parentElement: HTMLElement) {
         this.parentElement = parentElement;
-        this.sliderElement = new HTMLElement();
         this.initializeView();
     }
     initializeView() {
         this.sliderElement = document.createElement('div');
         this.sliderElement.classList.add('slider');
-        this.parentElement.append(this.sliderElement);
-        this.track = new Track(this.sliderElement);
-        this.sliderElement.append(this.track.getTrackElem);
+        this.attachChildElemToParent(this.sliderElement, this.parentElement);
+        this.track = new Track();
+        this.attachChildElemToParent(this.track.getElement(), this.sliderElement);
+        this.thumb = new Thumb();
+        this.attachChildElemToParent(this.thumb.getElement(), this.track.getElement());
+        this.progressBar = new ProgressBar();
+        this.attachChildElemToParent(this.progressBar.getElement(), this.track.getElement());
+
+    }
+    attachChildElemToParent(childElement: HTMLElement, parentElement: HTMLElement) {
+        parentElement.append(childElement);
     }
 }

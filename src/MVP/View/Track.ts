2586f4f4120430fb508846dc21beb4de51d1322a
Kamil <latipovkamil@mail.ru>
2021-09-02 20:03:34 +0300

    Added Track class

--- src/MVP/View/Track.ts
+++ src/MVP/View/Track.ts
@@ -1,5 +1,13 @@
 export class Track {
+    trackElem: HTMLElement;
     constructor() {
-
+        this.initializeTrack();
+    }
+    initializeTrack() {
+        this.trackElem = document.createElement('div');
+        this.trackElem.classList.add('slider__track');
+    }
+    getTrackElement() {
+        return (this.trackElem);
     }
 }
